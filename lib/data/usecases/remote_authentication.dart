27d2bbec1add97c34bad8f2499912c4f3745c397
frcc24 <frcc24@gmail.com>
2022-07-05 23:08:19 -0300

    removing API details from tests

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,6 +1,7 @@
-import 'package:lighthouse_delivery/screens/login/authentication.dart';
-
-import '../http/http_client.dart';
+import '../../domain//entities/entities.dart';
+import '../../domain/helpers/helpers.dart';
+import '../../domain/usecases/usecases.dart';
+import '../http/http.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -8,9 +9,17 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void>? auth(AuthenticationParams params) async {
+  Future<AccountEntity?> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
-    httpClient.request(url: url, method: 'post', body: body);
+    try {
+      final httpResponse =
+          await httpClient.request(url: url, method: 'post', body: body);
+      return AccountEntity.fromJson(httpResponse!);
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized
+          ? DomainError.invalidCredentials
+          : DomainError.unexpected;
+    }
   }
 }
 
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,7 +1,8 @@
 import 'package:faker/faker.dart';
-import 'package:lighthouse_delivery/data/http/http_client.dart';
+import 'package:lighthouse_delivery/data/http/http.dart';
 import 'package:lighthouse_delivery/data/usecases/remote_authentication.dart';
-import 'package:lighthouse_delivery/screens/login/authentication.dart';
+import 'package:lighthouse_delivery/domain/helpers/helpers.dart';
+import 'package:lighthouse_delivery/domain/usecases/authentication/authentication.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -29,5 +30,53 @@ void main() async {
       method: 'post',
       body: {'email': params.email, 'password': params.password},
     ));
+  });
+
+  test('Should throw UnexpectedError if returns 400', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final params =
+        AuthenticationParams(faker.internet.email(), faker.internet.password());
+
+    final future = await sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if returns 500', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.unauthorized);
+
+    final params =
+        AuthenticationParams(faker.internet.email(), faker.internet.password());
+
+    final future = await sut.auth(params);
+
+    expect(future, throwsA(DomainError.invalidCredentials));
+  });
+
+  test('Should return an Acc if returns 200', () async {
+    final accessToken = faker.guid.guid();
+
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenAnswer((_) async =>
+            {'accessToken': accessToken, 'name': faker.person.name()});
+
+    final params =
+        AuthenticationParams(faker.internet.email(), faker.internet.password());
+
+    final account = await sut.auth(params);
+
+    expect(account?.token, accessToken);
   });
 }
