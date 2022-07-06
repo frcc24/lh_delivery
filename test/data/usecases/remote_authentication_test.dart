ad32355ab866fb32b30edb5b41a0b20ccdcd6d24
frcc24 <frcc24@gmail.com>
2022-07-05 21:54:34 -0300

    removing API details from tests

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,24 +1,9 @@
 import 'package:faker/faker.dart';
+import 'package:lighthouse_delivery/data/http/http_client.dart';
+import 'package:lighthouse_delivery/data/usecases/remote_authentication.dart';
 import 'package:lighthouse_delivery/screens/login/authentication.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({required this.httpClient, required this.url});
-
-  Future<void>? auth(AuthenticationParams params) async {
-    final body = {'email': params.email, 'password': params.password};
-    httpClient.request(url: url, method: 'post', body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void>? request(
-      {required String url, required String method, Map body});
-}
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
