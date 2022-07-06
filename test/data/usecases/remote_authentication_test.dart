4ebe061ca504e4e0da1e6b7f8b1249bfad946ea7
frcc24 <frcc24@gmail.com>
2022-07-05 21:30:06 -0300

    implements body of http tests

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:lighthouse_delivery/screens/login/authentication.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -8,13 +9,15 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void>? auth() async {
-    httpClient.request(url: url, method: 'post');
+  Future<void>? auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password': params.password};
+    httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url, required String method});
+  Future<void>? request(
+      {required String url, required String method, Map body});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -31,11 +34,15 @@ void main() async {
   });
 
   test('Should call httpClient with correct values', () async {
-    await sut.auth();
+    final params =
+        AuthenticationParams(faker.internet.email(), faker.internet.password());
+
+    await sut.auth(params);
 
     verify(httpClient.request(
       url: url,
       method: 'post',
+      body: {'email': params.email, 'password': params.password},
     ));
   });
 }
